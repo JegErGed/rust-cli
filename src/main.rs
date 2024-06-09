71a3ffc1f26c7394eebd47d4c04047a82f587886
JegErGed <hoeckjohanged@gmail.com>
2024-06-09 14:28:11 +0200

    next step. Save it to a file.

--- src/main.rs
+++ src/main.rs
@@ -124,8 +124,8 @@ fn main() {
     println!("Welcome to your command line ToDo manager!\n\n");
     let mut db: DB = DB::new();
     loop {
-        println!("Type (+) to add a task.\nType (-) to remove a task.\nType (*) to remove all completed tasks.\nType (/) to change the status of a task\nType 'x' to exit.\n");
         db.print_db();
+        println!("Type (+) to add a task.\nType (-) to remove a task.\nType (*) to remove all completed tasks.\nType (/) to change the status of a task\nType 'x' to exit.\n");
         let choice: String = read!("{}\n");
 
         if choice.is_empty() {
@@ -150,27 +150,33 @@ fn main() {
             }
             '/' => {
                 println!("Choose the index of the task you want to change:");
-                let task_index: usize = read!();
-                if task_index > 0 && task_index <= db.0.len() {
-                    println!("Is this task completed? (y/n)");
-                    let answer: String = read!("{}\n");
-                    let ans: char = answer.chars().next().unwrap_or(' ');
-                    
-                    let status = match ans {
-                        'y' | 'Y' => true,
-                        'n' | 'N' => false,
-                        _ => {
-                            println!("Invalid input. Please enter 'y' or 'n'.");
-                            continue; // Skip the rest of the loop iteration and ask for input again
-                        }
-                    };
-                    
-                    db.update_task_status(task_index - 1, status);
-                } else {
-                    println!("Invalid task index.");
-                }
-                }
+                let task_index_input: String = read!("{}\n");
+                let task_index_input = task_index_input.trim();
 
+                if let Ok(task_index) = task_index_input.parse::<usize>() {
+                    if task_index > 0 && task_index <= db.0.len() {
+                        db.0[task_index-1].print_element();
+                        println!("Is this task completed? (y/n)");
+                        let answer: String = read!("{}\n");
+                        let ans: char = answer.chars().next().unwrap_or(' ');
+                    
+                        let status = match ans {
+                            'y' | 'Y' => true,
+                            'n' | 'N' => false,
+                            _ => {
+                                println!("Invalid input. Please enter 'y' or 'n'.");
+                                continue; // Skip the rest of the loop iteration and ask for input again
+                            }
+                        };
+                    
+                        db.update_task_status(task_index - 1, status);
+                    } else {
+                        println!("Invalid task index.");
+                    }
+                } else {
+                    println!("Invalid input. Please enter a valid number.");
+                }
+            }
             '*' => {
                 db.remove_completed_tasks();
             }
