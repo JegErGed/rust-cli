c08c6466006cbd572b603695dd932c4b43b6aba1
GustavZJ-WSL <gust3371@gmail.com>
2024-06-13 11:36:43 +0200

    Set up DB

--- GooseBank/src/main.rs
+++ GooseBank/src/main.rs
@@ -1,133 +1,23 @@
-use std::{
-    hash::{Hash, Hasher},
-    collections::hash_map::DefaultHasher,
-    ops::{Add, AddAssign, Sub, SubAssign},
-};
-
-#[derive(Clone, Debug, Hash)]
-enum Data {
-    Name(String),
-    Passwd(u64),
-    Money(i32),
-}
-
-struct User(Vec<Data>);
-
-impl Add<i32> for User {
-    type Output = User;
-
-    fn add(self, other: i32) -> User {
-        let mut new_entries = Vec::new();
-
-        for entry in self.0 {
-            match entry {
-                Data::Name(name) => new_entries.push(Data::Name(name)),
-                Data::Passwd(passwd) => new_entries.push(Data::Passwd(passwd)),
-                Data::Money(money) => new_entries.push(Data::Money(money + other)),
-            }
-        }
-
-        User(new_entries)
-    }
-}
-
-impl AddAssign<i32> for User {
-    fn add_assign(&mut self, other: i32) {
-        for entry in &mut self.0 {
-            if let Data::Money(money) = entry {
-                *money += other;
-            }
-        }
-    }
-}
-
-impl Sub<i32> for User {
-    type Output = User;
-
-    fn sub(self, other: i32) -> User {
-        let mut new_entries = Vec::new();
-
-        for entry in self.0 {
-            match entry {
-                Data::Name(name) => new_entries.push(Data::Name(name)),
-                Data::Passwd(passwd) => new_entries.push(Data::Passwd(passwd)),
-                Data::Money(money) => new_entries.push(Data::Money(money - other)),
-            }
-        }
-
-        User(new_entries)
-    }
-}
-
-impl SubAssign<i32> for User {
-    fn sub_assign(&mut self, other: i32) {
-        for entry in &mut self.0 {
-            if let Data::Money(money) = entry {
-                *money -= other;
-            }
-        }
-    }
-}
-
-impl User {
-    fn hash<T>(passwd: T) -> u64
-    where
-        T: Hash,
-    {
-        let mut hasher = DefaultHasher::new();
-        passwd.hash(&mut hasher);
-        hasher.finish()
-    }
-
-    fn new(name: String, passwd: String, money: i32) -> User {
-        let mut user = User(Vec::new());
-        user.0.push(Data::Name(name));
-        user.0.push(Data::Passwd(User::hash(passwd)));
-        user.0.push(Data::Money(money));
-        return user;
-    }
-
-    fn passwd(&self, input_pass: &str) -> bool {
-        let hashed_input = User::hash(input_pass);
-        for entry in &self.0 {
-            if let Data::Passwd(stored_passwd) = entry {
-                return *stored_passwd == hashed_input;
-            }
-        }
-        return false;
-    }
-
-    fn update_money(&mut self, add_money: i32, input_pass: &str) {
-        if !self.passwd(input_pass) {
-            println!("Access not granted!");
-            return;
-        }
-        for entry in &mut self.0 {
-            if let Data::Money(money) = entry {
-                *money += add_money;
-            }
-        }
-    }
-
-    fn print_user(&self, input_pass: &str) {
-        if !self.passwd(input_pass) {
-            println!("Access not granted!");
-            return;
-        }
-        for entry in &self.0 {
-            match entry {
-                Data::Name(name) => println!("Name: {}", name),
-                Data::Passwd(_) => (),
-                Data::Money(money) => println!("Money: {}", money),
-            }
-        }
-    }
-}
-
+mod data_struct;
+use data_struct::{Data, User};
 fn main() {
     println!("Welcome to GooseBank!");
 
     let mut user = User::new("Goulash".to_string(), "123".to_string(), 500);
+    let sdata = serde_json::to_string(&user);
+
+    if sdata.is_err() {
+        println!("Error, filed to Serialize structure {}", sdata.unwrap_err());
+        std::process::exit(1);
+    }
+
+    let sdata = sdata.unwrap();
+
+    println!("Serialized data: {}", sdata);
+
+    let mut f = fs::File::create("gooseBankDB.json").expect("Failed to create DB");
+    f.write_all(sdata.as_bytes()).expect("Failed to save to DB");
+
     user.print_user("123");
     user.update_money(200, "123");
     user.print_user("123");
